5215c135e93bf2b11fd872eddce89f1cfe544b58
Robert Gludo <robert_gludo@yahoo.com>
2020-03-21 23:04:55 +0800

    fix header navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,46 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {View, Image, ScrollView} from 'react-native';
 import IconButtonWithText from '../IconButtonWithText/IconButtonWithText';
 import styles from './styles';
-export default class Header extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.logo_icon}
-          source={require('../../assets/images/welove.png')}
+function Header({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo_icon}
+        source={require('../../assets/images/welove.png')}
+      />
+      <ScrollView horizontal={true}>
+        <IconButtonWithText
+          navigationItemIcon="home"
+          navigationItemTitle="Home"
         />
-        <ScrollView horizontal={true}>
-          <IconButtonWithText
-            navigationItemIcon="home"
-            navigationItemTitle="home"
-          />
-          <IconButtonWithText
-            navigationItemIcon="newspaper"
-            navigationItemTitle="Timeline"
-          />
-          <IconButtonWithText
-            navigationItemIcon="file"
-            navigationItemTitle="Report"
-          />
-          <IconButtonWithText
-            navigationItemIcon="brain"
-            navigationItemTitle="Intelligence"
-          />
-          <IconButtonWithText
-            navigationItemIcon="bullseye"
-            navigationItemTitle="Objective"
-          />
-          <IconButtonWithText
-            navigationItemIcon="building"
-            navigationItemTitle="Company"
-          />
-          <IconButtonWithText
-            navigationItemIcon="shopping-cart"
-            navigationItemTitle="Sale"
-          />
-        </ScrollView>
-      </View>
-    );
-  }
+        <IconButtonWithText
+          navigationItemIcon="newspaper"
+          navigationItemTitle="Timeline"
+        />
+        <IconButtonWithText
+          navigationItemIcon="file"
+          navigationItemTitle="Report"
+        />
+        <IconButtonWithText
+          navigationItemIcon="brain"
+          navigationItemTitle="Intelligence"
+        />
+        <IconButtonWithText
+          navigationItemIcon="bullseye"
+          navigationItemTitle="Objective"
+        />
+        <IconButtonWithText
+          navigationItemIcon="building"
+          navigationItemTitle="Company"
+        />
+        <IconButtonWithText
+          navigationItemIcon="shopping-cart"
+          navigationItemTitle="Sale"
+        />
+      </ScrollView>
+    </View>
+  );
 }
+
+export default Header;
--- src/components/IconButtonWithText/IconButtonWithText.js
+++ src/components/IconButtonWithText/IconButtonWithText.js
@@ -1,34 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {View, Text, TouchableOpacity} from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {useNavigation} from '@react-navigation/native';
 import iconButtonWithTextStyles from './styles';
 
-export default class IconButtonWithText extends Component {
-  _onPressButton() {
-    // eslint-disable-next-line no-alert
-    alert('You tapped the button');
-  }
-  render() {
-    return (
-      <View style={iconButtonWithTextStyles.navigationItem}>
-        <TouchableOpacity onPress={this._onPressButton}>
-          <View style={iconButtonWithTextStyles.navigationButton}>
-            <View style={iconButtonWithTextStyles.grayCircle}>
-              <FontAwesome5
-                name={this.props.navigationItemIcon}
-                size={25}
-                color="#6d6d6d"
-              />
-            </View>
-          </View>
-        </TouchableOpacity>
-        <Text style={iconButtonWithTextStyles.navigationButtonText}>
-          {this.Capitalize(this.props.navigationItemTitle)}
-        </Text>
-      </View>
-    );
-  }
-  Capitalize(str) {
+export default function IconButtonWithText({
+  navigationItemIcon,
+  navigationItemTitle,
+}) {
+  const Capitalize = str => {
     return str.charAt(0).toUpperCase() + str.slice(1);
-  }
+  };
+  const navigation = useNavigation();
+  return (
+    <View style={iconButtonWithTextStyles.navigationItem}>
+      <TouchableOpacity
+        onPress={() => navigation.navigate(navigationItemTitle)}>
+        <View style={iconButtonWithTextStyles.navigationButton}>
+          <View style={iconButtonWithTextStyles.grayCircle}>
+            <FontAwesome5 name={navigationItemIcon} size={25} color="#6d6d6d" />
+          </View>
+        </View>
+      </TouchableOpacity>
+      <Text style={iconButtonWithTextStyles.navigationButtonText}>
+        {Capitalize(navigationItemTitle)}
+      </Text>
+    </View>
+  );
 }
