cb0b55382d2743fcda9e1d4d5f551d7d55abe3d7
AndrLewis <60868470+AndrLewis@users.noreply.github.com>
2020-03-25 21:05:00 +0800

    Update Home.js
    Weekly update. Fix of weekly data.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,12 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {
   View,
-  Platform,
   StyleSheet,
   StatusBar,
   SafeAreaView,
   ScrollView,
-  TouchableOpacity,
   Text,
+  TouchableOpacity
 } from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import Moment from 'moment';
@@ -22,90 +20,108 @@ class HomeScreen extends React.Component {
     super(props);
     this.state = {
       user: {},
-      token: _retrieveData('token'),
+      token: '',
       dayplan: [],
       eodplan: [],
-      weeklyObjectives: [],
-      dayreports: [],
+      weeklyObljectives: [],
     };
   }
 
-  // expandView(type){
-  // }
+  expandView(type){
+
+  }
 
   componentDidMount() {
     let baseUrl = 'https://welove-intranet-backend.herokuapp.com';
     let userUrl = baseUrl + '/contas/id/';
     let reportUrl = baseUrl + '/reports/id/';
-    let objectivesUrl = baseUrl + '/objectives/weekNumber/';
     let userId;
-    let weekNumber = Moment().isoWeek();
     _retrieveData('user').then(user => this.setState({user: user}));
-    _retrieveData('token').then(token => {
-      this.setState({token: token});
-      fetch(objectivesUrl + weekNumber, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'x-access-token': token,
-          Authorization: 'Bearer ' + token,
-        },
-      })
-        .then(response => response.json())
-        .then(responseJson => {
-          //console.log(JSON.stringify(responseJson.data));
-          this.setState(prevState => ({
-            weeklyObjectives: [
-              ...prevState.weeklyObjectives,
-              responseJson.data,
-            ],
-          }));
-        });
-    });
+    _retrieveData('token').then(token => this.setState({token: token}));
 
-    let now = Moment();
-    let year = now.get('year');
-    let month = now.get('month') + 1;
-    let day = now.get('date');
-
-    fetch(baseUrl + '/reportsteam/year/' + year + '/month/' + month + '/day/' + day)
+    fetch(baseUrl + '/timelineentry/all')
       .then(response => response.json())
       .then(responseJson => {
-        let dayreports = responseJson.data;
-        dayreports.map(report => {
-          let url = '/reports/reportsteam/id/' + report._id;
-          fetch(baseUrl + url)
-            .then(response => response.json())
-            .then(responseReportJson => {
-              let reports = responseReportJson.data.sort(function(a, b) {
-                return new Date(b.createdAt) - new Date(a.createdAt);
-              });
-              reports.map(r => {
-                fetch(userUrl + r.userId)
-                  .then(response => response.json())
-                  .then(responseJsonUser => {
-                    if (responseJsonUser.success === true) {
-                      let user = responseJsonUser.data;
+        let day = [];
+        let eod = [];
 
+        day = responseJson.data.filter(
+          a => Moment(a.createdAt).startOf('day') - Moment().startOf('day') === 0 && a.type === 'dayplan'
+        );
+
+        eod = responseJson.data.filter(
+          a => Moment(a.createdAt).startOf('day') - Moment().startOf('day') === 0 && a.type === 'eod',
+        );
+
+        console.log('day: ' + JSON.stringify(day));
+        console.log('eod: ' + JSON.stringify(eod));
+
+
+        day.map(report => {
+          fetch(userUrl + report.userId)
+            .then(response => response.json())
+            .then(responseJsonUser => {
+              if (responseJsonUser.success === true) {
+                let user = responseJsonUser.data;
+                userId = user._id;
+
+                fetch(reportUrl + report.modelId, {
+                  headers: {
+                    'x-access-token': this.state.token,
+                  },
+                })
+                  .then(response => response.json())
+                  .then(responseJsonReport => {
+                    if (responseJsonReport.success === true) {
                       let reportItem = {
-                        userpic: user.picture,
-                        username: user.name,
-                        type: r.type,
-                        id: r._id,
-                        createdAt: r.createdAt,
+                        user: responseJsonUser.data,
+                        report: responseJsonReport.data,
+                        date: report.createdAt,
+                        id: report._id,
                       };
-                      if (r.type === 'dayplan') {
-                        this.setState(prevState => ({
-                          dayplan: [...prevState.dayplan, reportItem],
-                        }));
-                      } else {
-                        this.setState(prevState => ({
-                          eodplan: [...prevState.eodplan, reportItem],
-                        }));
-                      }
+                      this.setState(prevState => ({
+                        dayplan: [...prevState.dayplan, reportItem],
+                      }));
                     }
+                  })
+                  .catch(error => {
+                    console.error(error);
                   });
-              });
+              }
+            });
+        });
+
+        eod.map(report => {
+          fetch(userUrl + report.userId)
+            .then(response => response.json())
+            .then(responseJsonUser => {
+              if (responseJsonUser.success === true) {
+                let user = responseJsonUser.data;
+                userId = user._id;
+
+                fetch(reportUrl + report.modelId, {
+                  headers: {
+                    'x-access-token': this.state.token,
+                  },
+                })
+                  .then(response => response.json())
+                  .then(responseJsonReport => {
+                    if (responseJsonReport.success === true) {
+                      let reportItem = {
+                        user: responseJsonUser.data,
+                        report: responseJsonReport.data,
+                        date: report.createdAt,
+                        id: report._id,
+                      };
+                      this.setState(prevState => ({
+                        eodplan: [...prevState.eodplan, reportItem],
+                      }));
+                    }
+                  })
+                  .catch(error => {
+                    console.error(error);
+                  });
+              }
             });
         });
       });
@@ -114,10 +130,8 @@ class HomeScreen extends React.Component {
   render() {
     if (this.state.token) {
       return (
-        <View style={{
-          paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-        }}>
-          <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "transparent" translucent = {true}/>
+        <>
+          <StatusBar backgroundColor="transparent" />
           <SafeAreaView>
             <ScrollView
               contentInsetAdjustmentBehavior="automatic"
@@ -136,20 +150,23 @@ class HomeScreen extends React.Component {
                       }}>
                         <View style={styles.weeklyPart}>
                           <Text style={{fontSize: 17, borderBottomWidth: 1, borderColor: '#00000032' }}>Weekly Goals</Text>
-                          <View style={styles.progressStatus}>
-                            <View style={styles.objectiveProgress}>
-                              <Text style={styles.objectiveProgressTitle}>OBJECTIVE</Text>
-                              <View style={styles.objectiveProgressDescription}>
-                                <Text style={styles.objectiveProgressDescriptionContext}>Complete INTRANET mobile application Start Work on courses module Start work on timeline module</Text>
-                              </View>
-                            </View>
-                            <View style={styles.progressBar}>
-                              <Text style={styles.progressBarStatus}>Progress: 70%</Text>
-                              <ProgressBar value={70} maxValue={100} backgroundColorOnComplete="#123123" backgroundColor="#987987" />
-                            </View>
-                          </View>
                         </View>
                       </TouchableOpacity>
+                        <View style={styles.progressStatus}>
+                          <View style={styles.objectiveProgress}>
+                            <Text style={styles.objectiveProgressTitle}>OBJECTIVE</Text>
+                            <View style={styles.objectiveProgressDescription}>
+                              <Text style={styles.objectiveProgressDescriptionContext}>Complete INTRANET mobile application Start Work on courses module Start work on timeline module</Text>
+                              <Text>this.state.weeklyObljectives</Text>
+                            </View>
+                          </View>
+                          <View style={styles.progressBar}>
+                            <Text style={styles.progressBarStatus}>Progress: 70%</Text>
+                            <ProgressBar value={70} maxValue={100} backgroundColorOnComplete="#123123" backgroundColor="#987987" />
+                          </View>
+                        </View>
+
+
                       <TouchableOpacity onPress={()=>{
                         this.expandView("dop")
                       }}>
@@ -170,7 +187,7 @@ class HomeScreen extends React.Component {
               </View>
             </ScrollView>
           </SafeAreaView>
-        </View>
+        </>
       );
     } else {
       return <Login />;
@@ -192,7 +209,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
   },
   sectionContainer: {
-    marginTop: 24,
+    marginTop: 32,
     paddingHorizontal: 24,
   },
   sectionTitle: {
